fe6e4a9829847f674a6fb887fe4431b834d7f6a9
daniliuskosmaches <nasarspektrreas@gmail.com>
2025-12-16 00:10:51 +0500

    h

---
+++ src/shell.h
@@ -0,0 +1,8 @@
+//
+// Created by nazar on 16.12.25.
+//
+
+#ifndef UNTITLED2_SHELL_H
+#define UNTITLED2_SHELL_H
+
+#endif //UNTITLED2_SHELL_H
--- src/timer.h
+++ src/timer.h
@@ -1,21 +1,45 @@
 // src/timer.h
-
 #ifndef TIMER_H
 #define TIMER_H
 
-#include "idt.h" // Для registers_t
-#include "isr.h" // Для IRQ0
+#include "idt.h"
+#include "isr.h"
 
-// ---------------------------------------------------------------------
+// Глобальный счетчик тиков
+extern volatile unsigned long timer_ticks;
+
+// ============================================================
 // ПРОТОТИПЫ ФУНКЦИЙ ТАЙМЕРА
-// ---------------------------------------------------------------------
+// ============================================================
 
-// Основная функция для инициализации PIT (Programmable Interval Timer)
-void time_install(unsigned long freq);
+/**
+ * Инициализирует PIT (Programmable Interval Timer)
+ * @param frequency: Частота в Герцах (рекомендуется 100 Гц)
+ */
+void time_install(unsigned long frequency);
 
-// C-обработчик прерывания таймера (вызывается из isr_handler_c)
+/**
+ * C-обработчик прерывания таймера (IRQ0)
+ * Вызывается из isr_handler_c
+ */
 void timer_handler_c(registers_t *regs);
-extern void timer_handler_c(registers_t *regs);
-extern void timer_wait(unsigned long ticks);
 
-#endif
+/**
+ * Возвращает количество тиков с момента запуска
+ * @return: Количество тиков
+ */
+unsigned long timer_get_ticks(void);
+
+/**
+ * Простая функция задержки
+ * @param ticks: Количество тиков для ожидания
+ */
+void timer_wait(unsigned long ticks);
+
+/**
+ * Задержка в миллисекундах
+ * @param milliseconds: Количество миллисекунд
+ */
+void timer_sleep_ms(unsigned long milliseconds);
+
+#endif // TIMER_H
